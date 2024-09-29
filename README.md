02c5b4222a6bb4c2c41ca5f31f1cdc0c6e860262
Korovin Aleksandr <korovins94@yandex.ru>
2024-09-29 10:03:31 +0300

    first version

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Проектная работа Mesto
-
+https://github.com/Korovin-Aleksandr/mesto-project-ff.git
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,22 @@
 // @todo: Темплейт карточки
-
+const cardTemplate = document.querySelector('#card-template').content;
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
+const addCard = initialCards.forEach((element) => {
+  
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').textContent = 'alt';
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+ // @todo: Функция удаления карточки
+  deleteButton.addEventListener('click', function () {
+    const listItem = deleteButton.closest('.card');
+    listItem.remove();
+  });
 // @todo: Вывести карточки на страницу
+  placesList.append(cardElement);
+});
